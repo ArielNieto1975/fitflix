5b39efe3bd92bacd70e6e14342e971c21eb7d80d
Ariel <arielnieto1@hotmail.com>
2023-06-09 21:17:18 -0300

    copy

--- src/Componentes/Footer/index.js
+++ src/Componentes/Footer/index.js
@@ -1,9 +1,15 @@
 import "./Footer.css"
 const Footer = ()=>{
     return <div className="footer">
+        <div className="iconimg">
         <img className="fitdance" src="/img/logoFit.jpg" alt="Logo FitDance"/>
         <img className="fitflix" src="/img/logo-.png" alt="Logo Fitflix"/>
         <img className="fitdance" src="/img/logoFit.jpg" alt="Logo FitDance"/>
+        </div>
+        
+        <div class="copy">
+            <p>Developed by: Ariel Nieto © 2023</p>
+        </div>
     </div>
 }
 export default Footer
