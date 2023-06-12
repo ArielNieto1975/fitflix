bf2881034dc80903fcefa8b533984c3ae043ca4d
Ariel <arielnieto1@hotmail.com>
2023-06-11 21:47:28 -0300

    editar videos

--- src/Componentes/Body/FormNuevoVideo/index.js
+++ src/Componentes/Body/FormNuevoVideo/index.js
@@ -1,10 +1,104 @@
-import { useState } from "react"
-import "./formNuevoVideo.css"
-import { MdDelete} from 'react-icons/md';
-import CampoTexto from "../../CampoTexto"
-import ListaCategoria from "../../ListaCategoria"
-import { Boton } from "../../Boton"
-import { Boton2 } from "../../Boton"
+// import { useState } from "react"
+// import "./formNuevoVideo.css"
+// import { MdDelete, MdEdit } from 'react-icons/md';
+// import CampoTexto from "../../CampoTexto"
+// import ListaCategoria from "../../ListaCategoria"
+// import { Boton } from "../../Boton"
+// import { Boton2 } from "../../Boton"
+// import { Container } from '@mui/system';
+
+// const FormNuevoVideo = (props) => {
+//   const [titulo, setTitulo] = useState("");
+//   const [imagen, setImagen] = useState("");
+//   const [link, setLink] = useState("");
+//   const [categoria, setCategoria] = useState("");
+
+//   const { registrarVideo, videos, actualizarVideos } = props;
+
+//   const manejarEnvio = (e) => {
+//     e.preventDefault();
+//     let datosAEnviar = { titulo, link, categoria, imagen };
+//     registrarVideo(datosAEnviar);
+//     setTitulo("");
+//     setImagen("");
+//     setLink("");
+//     setCategoria("");
+//   };
+
+//   const eliminarVideo = (link) => {
+//     const nuevosVideos = videos.filter((video) => video.link !== link);
+//     actualizarVideos(nuevosVideos);
+//   };
+
+//   return (
+//     <Container component="section" maxWidth="md">
+//       <form onSubmit={manejarEnvio}>
+//         <h2 className="ingrese">Ingrese un Nuevo Video</h2>
+//         <ListaCategoria
+//           valor={categoria}
+//           actualizarCategoria={setCategoria}
+//           categorias={props.categorias}
+//         />
+//         <CampoTexto
+//           titulo="Titulo"
+//           placeHolder="Ingrese el titulo del Tema"
+//           valor={titulo}
+//           actualizarValor={setTitulo}
+//         />
+//         <CampoTexto
+//           titulo="Imagen"
+//           placeHolder="Ingrese imagen del Tema"
+//           valor={imagen}
+//           actualizarValor={setImagen}
+//         />
+//         <CampoTexto
+//           titulo="Link"
+//           placeHolder="Ingrese la URL del video"
+//           valor={link}
+//           actualizarValor={setLink}
+//         />
+//         <Boton value="Enviar" name="guardar" />
+//         <div className="newCat">
+//           <Boton2 value="Agregar Categoría" newCatOpen={props.newCatOpen} />
+//         </div>
+//       </form>
+//       <section className="video_cat">
+//         <h2 className="ingrese">Administrar Videos</h2>
+//         <div className="abm">
+//           {videos.map((vid) => (
+//             <div className="abm-item">
+//               <div className="icon1">
+//                 <MdEdit />
+//               </div>
+//               <div
+//                 className="icon2"
+//                 onClick={() => eliminarVideo(vid.link)}
+//               >
+//                 <MdDelete />
+//               </div>
+//               <img
+//                 className="vid-item"
+//                 alt="video"
+//                 key={vid.link}
+//                 src={vid.imagen}
+//               ></img>
+//             </div>
+//           ))}
+//         </div>
+//       </section>
+//     </Container>
+//   );
+// };
+
+// export default FormNuevoVideo;
+
+import { useState } from "react";
+import "./formNuevoVideo.css";
+import { MdDelete, MdEdit } from 'react-icons/md';
+import CampoTexto from "../../CampoTexto";
+import ListaCategoria from "../../ListaCategoria";
+import { Boton } from "../../Boton";
+import { Boton2 } from "../../Boton";
 import { Container } from '@mui/system';
 
 const FormNuevoVideo = (props) => {
@@ -12,13 +106,27 @@ const FormNuevoVideo = (props) => {
   const [imagen, setImagen] = useState("");
   const [link, setLink] = useState("");
   const [categoria, setCategoria] = useState("");
+  const [editing, setEditing] = useState(false); // new state to track if editing a video
+  const [videoToEdit, setVideoToEdit] = useState({}); // new state to store the video being edited
 
   const { registrarVideo, videos, actualizarVideos } = props;
 
   const manejarEnvio = (e) => {
     e.preventDefault();
     let datosAEnviar = { titulo, link, categoria, imagen };
-    registrarVideo(datosAEnviar);
+    if (editing) { // if editing a video, update it instead of registering a new one
+      const nuevosVideos = videos.map((video) => {
+        if (video.link === videoToEdit.link) {
+          return { ...video, ...datosAEnviar };
+        } else {
+          return video;
+        }
+      });
+      actualizarVideos(nuevosVideos);
+      setEditing(false);
+    } else {
+      registrarVideo(datosAEnviar);
+    }
     setTitulo("");
     setImagen("");
     setLink("");
@@ -30,34 +138,24 @@ const FormNuevoVideo = (props) => {
     actualizarVideos(nuevosVideos);
   };
 
+  const editarVideo = (video) => { // new function to handle editing a video
+    setVideoToEdit(video);
+    setTitulo(video.titulo);
+    setImagen(video.imagen);
+    setLink(video.link);
+    setCategoria(video.categoria);
+    setEditing(true);
+  };
+
   return (
     <Container component="section" maxWidth="md">
       <form onSubmit={manejarEnvio}>
         <h2 className="ingrese">Ingrese un Nuevo Video</h2>
-        <ListaCategoria
-          valor={categoria}
-          actualizarCategoria={setCategoria}
-          categorias={props.categorias}
-        />
-        <CampoTexto
-          titulo="Titulo"
-          placeHolder="Ingrese el titulo del Tema"
-          valor={titulo}
-          actualizarValor={setTitulo}
-        />
-        <CampoTexto
-          titulo="Imagen"
-          placeHolder="Ingrese imagen del Tema"
-          valor={imagen}
-          actualizarValor={setImagen}
-        />
-        <CampoTexto
-          titulo="Link"
-          placeHolder="Ingrese la URL del video"
-          valor={link}
-          actualizarValor={setLink}
-        />
-        <Boton value="Enviar" name="guardar" />
+        <ListaCategoria valor={categoria} actualizarCategoria={setCategoria} categorias={props.categorias} />
+        <CampoTexto titulo="Titulo" placeHolder="Ingrese el titulo del Tema" valor={titulo} actualizarValor={setTitulo} />
+        <CampoTexto titulo="Imagen" placeHolder="Ingrese imagen del Tema" valor={imagen} actualizarValor={setImagen} />
+        <CampoTexto titulo="Link" placeHolder="Ingrese la URL del video" valor={link} actualizarValor={setLink} />
+        <Boton value={editing ? "Guardar cambios" : "Guardar"} name="guardar" />
         <div className="newCat">
           <Boton2 value="Agregar Categoría" newCatOpen={props.newCatOpen} />
         </div>
@@ -67,18 +165,13 @@ const FormNuevoVideo = (props) => {
         <div className="abm">
           {videos.map((vid) => (
             <div className="abm-item">
-              <div
-                className="icon"
-                onClick={() => eliminarVideo(vid.link)}
-              >
+              <div className="icon1">
+                <MdEdit onClick={() => editarVideo(vid)} /> {/* add edit icon */}
+              </div>
+              <div className="icon2" onClick={() => eliminarVideo(vid.link)}>
                 <MdDelete />
               </div>
-              <img
-                className="vid-item"
-                alt="video"
-                key={vid.link}
-                src={vid.imagen}
-              ></img>
+              <img className="vid-item" alt="video" key={vid.link} src={vid.imagen}></img>
             </div>
           ))}
         </div>
--- src/Componentes/Header/index.js
+++ src/Componentes/Header/index.js
@@ -5,8 +5,8 @@ const Header = (props)=>{
     
     return <div className="header">
             <img src="img/logo-.png" alt="Logo Fitflix" onClick={props.mostrarPrincipal}/>
-            { props.mostrarNewVid || props.mostrarNewCat? <></> : <Boton value="Nuevo Video" cambiarMostrarNV={props.cambiarMostrarNV}/> }
-            { props.Principal? <Boton className="boton_princ" value="Nuevo Video" cambiarMostrarNV={props.cambiarMostrarNV}/>  : <></> }
+            { props.mostrarNewVid || props.mostrarNewCat? <></> : <Boton value="ABM Videos" cambiarMostrarNV={props.cambiarMostrarNV}/> }
+            { props.Principal? <Boton className="boton_princ" value="ABM Videos" cambiarMostrarNV={props.cambiarMostrarNV}/>  : <></> }
         </div>
 }
 export default Header
